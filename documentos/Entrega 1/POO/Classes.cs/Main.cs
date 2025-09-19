124073b7ffb09b06a3de96ec3601a605b534e797
Samuel Cavalcanti Nunes Coronel <samcronwell@gmail.com>
2025-09-19 20:43:00 -0300

    entrega POO de classes
    criei duas classes adicionando algumas funcionalidades na Main.

---
+++ documentos/Entrega 1/POO/Classes.cs/Main.cs
@@ -0,0 +1,32 @@
+﻿using MeuProjeto_alma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Admin admin1 = new Admin();
+        Usuario user1 = new Usuario();
+        Console.WriteLine("digite sua senha");
+        //entrada de admin//
+        int senhaadmin = Convert.ToInt32(Console.ReadLine());
+        int senhacorreta = 040604;
+
+        if (senhaadmin == senhacorreta)
+        {
+            Console.WriteLine("senha correta");
+        }
+        else
+        {
+            Console.WriteLine("senha incorreta");
+        }
+        admin1.SetId(1);
+        admin1.SetNome("Samuel Coronel");
+        admin1.SetEmail("samuel@email.com");
+        admin1.SetSenha(Convert.ToInt32("1234")); 
+        admin1.mostrar_info();
+
+
+        user1.SetNome("Maria");
+        user1.SetEmail("maria@email.com");
+    }
+}
---
+++ documentos/Entrega 1/POO/Classes.cs/Usuario.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace MeuProjeto_alma
+{
+    public class Usuario
+    {
+        public bool IsAdm;
+        private int id;
+        private string nome;
+        private string email;
+        private int senha;
+
+        public String GetNome()
+        {
+            return nome;
+        }
+        public void SetNome(String NovoNome)
+        {
+            nome = NovoNome;
+        }
+        public String GetEmail()
+        {
+            return email;
+        }
+        public void SetEmail(string NovoEmail)
+        {
+            email = NovoEmail;
+        }
+        public int GetSenha()
+        {
+            return senha;
+        }
+        public void SetSenha(int NovaSenha)
+        {
+            senha = NovaSenha;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetId(int NovoId)
+        {
+            id = NovoId;
+        }
+        public bool GetIsAdm()
+        {
+            return IsAdm;
+        }
+        public void SetIsAdm(bool NovoIsAdm)
+        {
+            IsAdm = NovoIsAdm;
+        }
+    }
+}
